1fc7772bd07809e4b644919f294a4ceab8397c84
morblom <morblom@atex.com>
2018-09-06 15:15:35 +0200

    Adds dependencies to metadata.rb and Berksfile. Adds some info to metadata.

--- Berksfile
+++ Berksfile
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 source 'https://supermarket.chef.io'
 
+cookbook 'wait_for, git: git@github.com:atex-polopoly/wait_for'
+cookbook 'aws, git: git@github.com:atex-polopoly/aws'
 metadata
--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,10 @@
 name 'terminator'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Atex Managed Services'
+maintainer_email 'managed-services@atex.com'
 license 'All Rights Reserved'
-description 'Installs/Configures terminator'
-long_description 'Installs/Configures terminator'
-version '0.1.0'
+description 'Terminates servers based on disk %.'
+version '1.0.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/terminator/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/terminator'
+depends 'aws'
+depends 'wait_for'
